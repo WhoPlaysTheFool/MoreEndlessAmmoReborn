4e09101a97489327d42ed9cd0dfbb1dc342e7468
Katelin <greencarsarefast@gmail.com>
2025-01-17 10:45:45 +1100

    Add endless flaming quiver
    crafted with regular endless quiver and magma stone at a workbench

---
+++ Items/EndlessFlamingQuiver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreEndlessAmmoReborn.Items
+{
+	public class EndlessFlamingQuiver : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("Endless Flaming Quiver");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.shootSpeed = 5f;
+			Item.shoot = 2;
+			Item.damage = 7;
+			Item.width = 26;
+			Item.height = 26;
+			Item.ammo = AmmoID.Arrow;
+			Item.knockBack = 2f;
+			Item.value = Item.sellPrice(0, 2, 0, 0);
+			Item.DamageType = DamageClass.Ranged;
+			Item.rare = 7;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.EndlessQuiver, 1);
+			recipe.AddIngredient(ItemID.MagmaStone, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
